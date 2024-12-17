2abff66bf4a487cac31689806888201a5d0b70e9
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2024-12-17 15:02:07 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+// sw.js
+
+self.addEventListener('install', function(event) {
+    // Cache music files and other assets here
+});
+
+self.addEventListener('fetch', function(event) {
+    // Serve cached files when offline
+});
